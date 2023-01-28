3c8933f216c46c953c8eb592ab357c3fd0cc55e7
Elizaveta <hamlizuha@mail.ru>
2023-01-29 00:28:53 +0300

    Почистила код в классе Task1.java от промежуточных действий в результате отладки кода

--- Task1.java
+++ Task1.java
@@ -4,14 +4,12 @@
    SELECT * FROM students WHERE name = "Ivanov" AND country = "Russia" AND city = "Moscow";
    Для разбора строки используйте String.split. Сформируйте новую строку, используя StringBuilder.
    Значения null не включаются в запрос.
- */
-/* Структура программы (создать методы):
+
+   Структура программы (создать методы):
     1. Получить строку на основе данных из файла
     3. Разбор строки (удаление лишних символов)
     4. Создание новой строки в форме SQL-запроса
-
 */
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,8 +18,6 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        //String str = getStringFromFile("Task1.txt");
-        //System.out.println("Строка из файла: " + str);
         createStringSqlRequest(getSplitString(getStringFromFile("Task1.txt")));
     }
     static String getStringFromFile(String fName) {
@@ -39,7 +35,7 @@ public class Task1 {
         String str1 = str.substring(1, str.length() - 1); //"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"
         String[] arrStr = str1.split(", ");//["name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"]
         int size = arrStr.length;
-        ArrayList<String> parametres = new ArrayList<String>(); //ArrayList тут использую для того, чтобы не заморачиваться с размером массива
+        ArrayList<String> parametres = new ArrayList<String>(); //ArrayList использую чтобы не заморачиваться с размером массива
         for (int i = 0; i < size; i++) {                        // также удобно добавлять нужные элементы в массив
             String[] param = arrStr[i].split(":");
             if (!param[1].contains("null")) { //если "значение" не равно "null"
@@ -55,7 +51,6 @@ public class Task1 {
         int size = values.length;
         StringBuilder sbSQL = new StringBuilder();
         sbSQL.append("SELECT * FROM students WHERE ");
-        //System.out.println(size);
         int count = 0;
         for (int i = 0; i < size - 2; i+=2) {
             sbSQL.append(values[i],1,values[i].length()-1).append(" = ").append(values[i+1]).append(" AND ");
